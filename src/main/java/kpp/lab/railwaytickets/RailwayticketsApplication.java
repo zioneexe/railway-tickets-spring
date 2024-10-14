b75a229bfd17d2f781617c82da229d378dce7fba
zioneeeeeee <w3st.nt@gmail.com>
2024-10-14 19:25:03 +0300

    project setup

---
+++ src/main/java/kpp/lab/railwaytickets/RailwayticketsApplication.java
@@ -0,0 +1,13 @@
+package kpp.lab.railwaytickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayticketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayticketsApplication.class, args);
+	}
+
+}
