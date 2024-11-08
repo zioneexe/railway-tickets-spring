4e4bf68c40795f4f9cfdf8de5c5403188ab3585c
zioneeeeeee <w3st.nt@gmail.com>
2024-11-08 02:46:43 +0200

    order logging added

--- src/main/java/kpp/lab/railwaytickets/services/OrderServiceImpl.java
+++ src/main/java/kpp/lab/railwaytickets/services/OrderServiceImpl.java
@@ -5,22 +5,40 @@ import kpp.lab.railwaytickets.model.BaseOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Процес обслуговування клієнта повинен бути зафіксований в журналі логування
+ * з зазначеним номером каси, яка обслуговувала клієнта,
+ * унікальним номером клієнта
+ * та стартовим і кінцевим часом обслуговування клієнта біля каси.
+ */
 @Service
 public class OrderServiceImpl implements OrderService {
 
-    private List<BaseLogger> orders;
+    private final List<BaseLogger> orders;
 
-    private BaseLogger logger;
+    private final BaseLogger logger;
 
     @Autowired
     public OrderServiceImpl(BaseLogger logger) {
-
+        this.logger = logger;
+        this.orders = new LinkedList<>();
     }
 
     @Override
     public void processOrder(BaseOrder order) {
 
+        logOrder(order);
+    }
+
+    private void logOrder(BaseOrder order) {
+        logger.log("--order--");
+        logger.log("Client: " + order.getClient().getId());
+        logger.log("Tickets count: " + order.getTicketsCount());
+        logger.log("Cash desk: " + order.getCashDesk().getId());
+        logger.log("Start time:" + order.getStartTime());
+        logger.log("End time:" + order.getEndTime());
     }
 }
