10e0668c4d8f4ea6559de7bb7cbd0880c30e1b66
O-Razik <razik8bit@gmail.com>
2024-11-23 17:51:28 +0200

    Corses fix try #1

--- src/main/java/kpp/lab/railwaytickets/controllers/OrderController.java
+++ src/main/java/kpp/lab/railwaytickets/controllers/OrderController.java
@@ -9,14 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/order")
 public class OrderController {
 
--- src/main/java/kpp/lab/railwaytickets/controllers/SettingsController.java
+++ src/main/java/kpp/lab/railwaytickets/controllers/SettingsController.java
@@ -12,15 +12,13 @@ import kpp.lab.railwaytickets.model.Position;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/settings")
 public class SettingsController {
 
